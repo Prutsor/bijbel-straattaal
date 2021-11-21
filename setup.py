d4f7db64c5eadeb2d759604a9a6db87a4283f96c
Prutsor <maxkoster2007@gmail.com>
2021-11-21 17:32:41 +0100

    new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'straattaal_bijbel',         # How you named your package folder (MyLib)
   packages = ['straattaal_bijbel'],   # Chose the same as "name"
-  version = '349058290485923485902349023849082390582905802938409238409823403495893485938904859304859083940850938495384958398267438957638945763459867349856789345768973459867893457689347689734598673495867394586739480506793845769384573490582904859234859023490238490823905829058029384092384098234034958934859389048593048590839408509384953849583982674389576389457634598673498567893457689734598678934576893476897345986734958673945867394805067938457693845798234.0',      # Start with a small number and increase it with every change you make
+  version = '97867967678689734589734258973489579438579384579384.0',      # Start with a small number and increase it with every change you make
   license='Apache 2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'God',                   # Type in your name
